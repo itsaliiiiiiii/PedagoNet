b3044b1ffb3bb1d0c8d36805a5cd52eb24f29bb0
ali harit <111861191+itsaliiiiiiii@users.noreply.github.com>
2025-04-24 15:03:45 +0100

    Fix : Database Connection

--- Backend/src/repositories/base.repository.js
+++ Backend/src/repositories/base.repository.js
@@ -1,12 +1,9 @@
-const neo4j = require('neo4j-driver');
 require('dotenv').config();
+const { neo4jDriver } = require('../config/database')
 
 class BaseRepository {
     constructor() {
-        this.driver = neo4j.driver(
-            process.env.NEO4J_URI,
-            neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
-        );
+        this.driver = neo4jDriver;
     }
 
     async getSession() {
